bec9ac2940f1ee8b6be4ac565f1a4de7bd2236d7
Juan Carvajal <jmcr-@hotmail.com.ar>
2025-09-30 20:15:08 -0500

    refactor: update AppTheme to use Poppins font and create ThemeProvider for theme management

--- lib/core/theme/app_theme.dart
+++ lib/core/theme/app_theme.dart
@@ -4,8 +4,20 @@ import 'package:google_fonts/google_fonts.dart';
 class AppTheme {
   static ThemeData get lightTheme {
     return ThemeData(
-      colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      textTheme: GoogleFonts.montserratTextTheme(
+      colorScheme: ColorScheme.fromSeed(seedColor: Colors.red),
+      textTheme: GoogleFonts.poppinsTextTheme(
+        const TextTheme(
+          bodyLarge: TextStyle(fontSize: 16.0),
+          bodyMedium: TextStyle(fontSize: 14.0),
+        ),
+      ),
+    );
+  }
+
+  static ThemeData get darkTheme {
+    return ThemeData(
+      colorScheme: ColorScheme.fromSeed(seedColor: Colors.orange),
+      textTheme: GoogleFonts.poppinsTextTheme(
         const TextTheme(
           bodyLarge: TextStyle(fontSize: 16.0),
           bodyMedium: TextStyle(fontSize: 14.0),
