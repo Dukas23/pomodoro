d17f6839fd3d8722b91e8fa46db1d978685edb62
Juan Carvajal <jmcr-@hotmail.com.ar>
2025-09-30 20:15:29 -0500

    refactor: enhance PomodoroScreen with theme toggle functionality and improve code consistency

--- lib/features/pomodoro/promodor_screen.dart
+++ lib/features/pomodoro/promodor_screen.dart
@@ -3,6 +3,8 @@ import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:pomodoro/features/pomodoro/widgets/circular_progress_with_contex.dart';
 import 'package:pomodoro/core/utils/pomodoro_timer.dart';
+import 'package:provider/provider.dart';
+import 'package:pomodoro/core/theme/theme_provider.dart';
 
 class PomodoroScreen extends StatefulWidget {
   const PomodoroScreen({super.key});
@@ -43,25 +45,40 @@ class PomodoroScreenState extends State<PomodoroScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: const Text('Pomodoro'),
+        actions: [
+          IconButton(
+            icon: Icon(
+              Provider.of<ThemeProvider>(context).isDarkMode
+                  ? Icons.dark_mode
+                  : Icons.light_mode,
+            ),
+            onPressed: () {
+              Provider.of<ThemeProvider>(context, listen: false).toggleTheme();
+            },
+          ),
+        ],
+      ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             Text(
               pomodoroTimer.currentType.name.toUpperCase(),
-              style: TextStyle(fontWeight: FontWeight.w900),
+              style: const TextStyle(fontWeight: FontWeight.w900),
             ),
-            SizedBox(height: 20.0),
+            const SizedBox(height: 20.0),
             CircularProgressWithContex(
               value: pomodoroTimer.seconds / 1500,
               text: Text(
                 '${(pomodoroTimer.seconds ~/ 60).toString().padLeft(2, '0')}:${(pomodoroTimer.seconds % 60).toString().padLeft(2, '0')}',
-                style: TextStyle(fontWeight: FontWeight.bold),
+                style: const TextStyle(fontWeight: FontWeight.bold),
               ),
               strokeWidth: 10.0,
               size: 300.0,
             ),
-            SizedBox(height: 10.0),
+            const SizedBox(height: 10.0),
             FloatingActionButton(
               onPressed: toggleTimer,
               child: Icon(
